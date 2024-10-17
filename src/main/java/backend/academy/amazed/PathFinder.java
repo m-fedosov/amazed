674a52b8db1ec08abf57c548afaca3d3bd1ed57c
m-fedosov <mifedosov@miem.hse.ru>
2024-10-17 17:09:45 +0300

    style: friendship with linter 2

--- src/main/java/backend/academy/amazed/PathFinder.java
+++ src/main/java/backend/academy/amazed/PathFinder.java
@@ -7,10 +7,18 @@ public interface PathFinder {
 
     default ArrayList<MazeGrid.Cell> getNeighbours(MazeGrid mazeGrid, MazeGrid.Cell cell) {
         ArrayList<MazeGrid.Cell> neighbours = new ArrayList<>();
-        if (!cell.northWall()) neighbours.add(mazeGrid.getCell(cell.y() - 1, cell.x()));
-        if (!cell.southWall()) neighbours.add(mazeGrid.getCell(cell.y() + 1, cell.x()));
-        if (!cell.westWall()) neighbours.add(mazeGrid.getCell(cell.y(), cell.x() - 1));
-        if (!cell.eastWall()) neighbours.add(mazeGrid.getCell(cell.y(), cell.x() + 1));
+        if (!cell.northWall()) {
+            neighbours.add(mazeGrid.getCell(cell.y() - 1, cell.x()));
+        }
+        if (!cell.southWall()) {
+            neighbours.add(mazeGrid.getCell(cell.y() + 1, cell.x()));
+        }
+        if (!cell.westWall()) {
+            neighbours.add(mazeGrid.getCell(cell.y(), cell.x() - 1));
+        }
+        if (!cell.eastWall()) {
+            neighbours.add(mazeGrid.getCell(cell.y(), cell.x() + 1));
+        }
         return neighbours;
     }
 }
